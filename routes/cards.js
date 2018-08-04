0679191c46bbbccafe8cac410e91fa32dee001ee
Jitendr Choudhary <Jitendra93266@gmail.com>
2018-08-04 18:53:38 +0530

    Query String

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 /*
 const data=require('../data/flashcardData.json').data;
 const cards=data.cards;
 */
-const {data}=require('../data/flashcardData.json');
-const {cards}=data;
-router.get('/:id', (req, res) => {
-    const id= req.params.id;
-    res.render('card', { prompt: cards[id].question,hint:cards[id].hint });
+const { data } = require("../data/flashcardData.json");
+const { cards } = data;
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const { side } = req.query;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const templateData = { text };
+  if (side == "answer") {
+    templateData.hint = hint;
+  }
+  res.render("card", templateData);
 });
 
 module.exports = router;

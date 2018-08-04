ae85b3a73f69759703a401fae1fff59034ebb770
Jitendr Choudhary <Jitendra93266@gmail.com>
2018-08-04 18:37:23 +0530

    Routing using paramater

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+/*
+const data=require('../data/flashcardData.json').data;
+const cards=data.cards;
+*/
+const {data}=require('../data/flashcardData.json');
+const {cards}=data;
+router.get('/:id', (req, res) => {
+    const id= req.params.id;
+    res.render('card', { prompt: cards[id].question,hint:cards[id].hint });
 });
 
 module.exports = router;
